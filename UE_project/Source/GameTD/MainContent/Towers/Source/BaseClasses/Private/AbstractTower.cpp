83142e515982a39cc1f51a86f81faa6bcd35e7dd
Mysar <melnichenko.d@theinvaders.pro>
2024-07-02 00:29:35 +0300

    Create abstract class player

--- UE_project/Source/GameTD/MainContent/Towers/Source/BaseClasses/Private/AbstractTower.cpp
+++ UE_project/Source/GameTD/MainContent/Towers/Source/BaseClasses/Private/AbstractTower.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AbstractTower.h"
+#include "../Public/AbstractTower.h"
 
 // Sets default values
 AAbstractTower::AAbstractTower()
