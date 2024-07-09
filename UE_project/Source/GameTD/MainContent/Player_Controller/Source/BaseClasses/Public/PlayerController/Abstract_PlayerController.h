e2fca5c730d19c0c1f87f8681b1a56a3d8da570d
KOST9K <iul.02@mail.ru>
2024-07-09 22:06:48 +0300

    Revert "Merge branch 'main' into Code/Tower/Create-Abstract-Tower"
    This reverts commit d5d6bbb593202cd03026ee8d1dd8ee19c9dd11e9, reversing
    changes made to 909b17bb517510764407e3c02dd09f6035ff3049.

---
+++ UE_project/Source/GameTD/MainContent/Player_Controller/Source/BaseClasses/Public/PlayerController/Abstract_PlayerController.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Abstract_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMETD_API AAbstract_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	//AAbstract_PlayerController();
+
+	FString PlayerNick;
+	float PlayerHitPoints;
+	int Money;
+protected:
+	virtual void MoveForward(const float Value){};
+	virtual void MoveRight(const float Value){};
+	virtual void Jump(){};
+	virtual void Shoot(){};
+	
+	virtual void EventDie(){};
+	virtual void EventLevelUp(){};
+	virtual void GetWeapon(){};
+	virtual void DropWeapon(){};
+};
--- UE_project/Source/GameTD/Private/Player_Controller/Source/RegularClasses/Base_PlayerController.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Player_Controller/Source/RegularClasses/PlayerController/Base_PlayerController.h"
-#include "Player_Controller/Source/BaseClasses/PlayerController/Abstract_PlayerController.h"
-
-ABase_PlayerController::ABase_PlayerController()
-	: ::AAbstract_PlayerController(),CharacterType(TEXT("Name"))
-{
-}
